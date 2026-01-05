d680ea0fe4c7cfe971c5917cb3bf48d13859e1e4
Chris Evans <chris.muttley.evans@gmail.com>
2026-01-05 11:07:15 +0000

    wip: Add deployment workflows

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://anitorious.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
@@ -24,6 +24,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "Anitorious", // Usually your GitHub org/user name.
   projectName: "sparkle-engine-wiki", // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
 
